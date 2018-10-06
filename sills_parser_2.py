4a19bd8b31ebc865a1f3e092031f74857024af7e
Murray Hoggett <murrayhoggett@gmail.com>
2018-10-06 13:01:16 +0100

    Finished sills_parser.py, and commited output csv files ready for stats and R plotting. Sills parser may still have minor bugs, worth cross checking with the seismic.

--- sills_parser_2.py
+++ sills_parser_2.py
@@ -10,24 +10,37 @@ of sill horizons from 2D seismic, and return the:
 2. Diameter of the sill 
 3. Maximum transgressive height 
 4. Maximum emplacement depth 
+
+Note, this command: sed -i 's/./& /47' run on oga seafloor first to add space
+for fixed width text files. 
+Also ran sed -i '$ d' to remove final line (which reads "EOD")
 """
 
 
-##### Currently the logic is for seafloor, but am giving it ff !! 
+### Uncomment for OGA data
+#file = "OGA_sills.dat"
+#ff_file = "oga_forced_folds.dat"
+#sf_file = "oga_seafloor.dat"
 
-file = "OGA_sills.dat"
-ff_file = "oga_forced_folds.dat"
+### Uncomment for PAD data
+file = "all_sills_PAD_ireland.dat"
+ff_file = "pad_forced_folds.dat"
+sf_file = "seafloor_PAD_ireland.dat"
 
 import math 
 import numpy as np 
 import pandas as pd
 
-# Read in forced folds and seafloor 
+## Read in forced folds and seafloor 
 ff = np.loadtxt(ff_file, skiprows=2, usecols=(0,1,2))
+sf = np.loadtxt(sf_file, skiprows=2, usecols=(0,1,2))
 
 # Sort forced folds and seafloor 
 ff = ff[ff[:,1].argsort(),:]
 ff = ff[ff[:,0].argsort(kind="mergesort"),:]
+
+sf = sf[sf[:,1].argsort(),:]
+sf = sf[sf[:,0].argsort(kind="mergesort"),:]
 
 
 file_connection = open(file, "r")
@@ -43,21 +56,83 @@ def fixed_width(line):
         return float(c1), float(c2), float(c3)
     except:
         raise ValueError("Fixed width exception!")
-
-def depth_con(s_x, s_y, s_time):
+        
+def ff_time(s_x, s_y):
     '''
-    Depth convert a sill time. Uses binary search and hence requires
-    ff (the forced fold) to be sorted beforehand. Uses depth below 
+    Find and return the time to the overlying forced fold horizon.
+    Uses binary search and hence requires the ff to be sorted beforehand.
+    The try / except is for where sills are not within the forced fold 
+    horizon - best to drop the sill rather than extrapolate. 
+    '''
+    # Find time to ff
+    x_l = np.searchsorted(ff[:,0], s_x, side="left")
+    x_r = np.searchsorted(ff[:,0], s_x, side="right")
+    if x_l == x_r:
+        try:
+            return ff[x_l, 2]
+        except IndexError:
+            return np.nan        
+    else:
+        # Remember to add x_l, as next line only searches sub array. 
+        ix = np.searchsorted(ff[x_l:x_r,1], s_y, side="right") + x_l
+        try:
+            return ff[ix, 2]
+        except IndexError:
+            return np.nan
+    
+
+def depth_con(s_x, s_y, time):
+    '''
+    Depth convert a twtt time. Uses binary search and hence requires
+    sf (the seafloor) to be sorted beforehand. Uses depth below 
     seafloor - time relationship from thesis. 
     '''
     # Find time below seafloor 
-    x_l = np.searchsorted(ff[:,0], s_x, side="left")
-    x_r = np.searchsorted(ff[:,0], s_x, side="right")
-    ix = np.searchsorted(ff[x_l:x_r,1], s_y, side="right")
-    sf_time = ff[ix, 2]
-    t_below_sf = s_time/1000.0 - sf_time/1000.0
-    # Return depth converted value 
-    return -0.04 + 1.08 * t_below_sf + 0.13 * t_below_sf**2
+    x_l = np.searchsorted(sf[:,0], s_x, side="left")
+    x_r = np.searchsorted(sf[:,0], s_x, side="right")
+    if x_l == x_r:
+        try:
+            sf_time = sf[x_l, 2]
+            t_below_sf = time/1000.0 - sf_time/1000.0
+            # Return depth converted value 
+            return -0.04 + 1.08 * t_below_sf + 0.13 * t_below_sf**2
+        except IndexError:
+            return np.nan    
+    else:
+        ix = np.searchsorted(sf[x_l:x_r,1], s_y, side="right") + x_l
+        try:
+            sf_time = sf[ix, 2]
+            t_below_sf = time/1000.0 - sf_time/1000.0
+            # Return depth converted value 
+            return -0.04 + 1.08 * t_below_sf + 0.13 * t_below_sf**2
+        except IndexError:
+            return np.nan
+
+def decompact(z1, z2, z3, c=0.51, phi_0=0.51, tolerance=0.001):
+    '''
+    This function implements the decompaction function in allen and allen 2005.
+    z1 is the depth to the present day top of the package, in km. z2 is the depth to
+    the present day base of the package, in km. z3 is the depth to which the top of the
+    package is being moved to by the decompaction. The function returns z4, the depth to the 
+    base of the package. 
+    c is the porosity-depth coefficient, in km**-1. representative values are shale:0.51, 
+    sst:0.27, chalk:0.71, shaly sand: 0.39
+    phi_0 is the surface porosity. Representative values are shale:0.63, sst:0.49, 
+    chalk:0.7, shaly sand: 0.56. 
+    '''
+    import numpy as np
+    assert type(z1) == int or type(z1) == float or type(z1) == np.float64, "Type of z1 must be an int or a float. z1 = " + str(z1) + ". Type = " + str(type(z1))
+    assert type(z2) == int or type(z2) == float or type(z1) == np.float64, "Type of z2 must be an int or a float"
+    assert type(z3) == int or type(z3) == float or type(z1) == np.float64, "Type of z3 must be an int or a float"
+    z4_new = z2-z1    
+    z4_old = (z2 - z1) + 4*tolerance
+    safety_counter = 0
+    while(abs(z4_old - z4_new) > tolerance and safety_counter < 1000):
+        z4_old = z4_new
+        safety_counter += 1
+        z4_new = z2 - z1 - (phi_0 / c)*(np.exp(-c*z1) - np.exp(-c*z2)) + (phi_0/c)*(np.exp(-c*z3) - np.exp(-c*z4_old))
+    return z4_new
+
 
 x, y, z, x_min, x_max, y_min, y_max = np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
 z_list = []
@@ -76,6 +151,11 @@ for line in file_connection:
             sill_min_t = np.min(z_list)
             mid_x = (x_min + x_max) / 2.0
             mid_y = (y_min + y_max) / 2.0
+            sill_max_depth = depth_con(mid_x, mid_y, sill_max_t)
+            ff_max_time = ff_time(mid_x, mid_y)
+            ff_max_depth = depth_con(mid_x, mid_y, ff_max_time)
+            decomp_emplac_depth = decompact(ff_max_depth, sill_max_depth, z3=0)
+            emplacement_depth.append(decomp_emplac_depth)
             midpoint_x.append(mid_x)
             midpoint_y.append(mid_y)
             diameter.append(math.sqrt((x_max - x_min)**2 + (y_max - y_min)**2))
@@ -97,6 +177,23 @@ for line in file_connection:
 
 file_connection.close() 
 
-data = pd.DataFrame({"midpoint_x": midpoint_x, "midpoint_y":midpoint_y, "diameter":diameter, "transgressive_height":trans_height})
-data.to_csv("output.csv")
+data = pd.DataFrame({"name":sill_names, "midpoint_x": midpoint_x, "midpoint_y":midpoint_y, "diameter":diameter, "transgressive_height":trans_height, "emplacement_depth":emplacement_depth})
+# Convert inf or -inf to na, then drop all rows with na. 
+data = data.replace([np.inf, -np.inf], np.nan).dropna()
+# If any emplacement depths are negative something is wrong, so drop
+data = data[data.emplacement_depth > 0]
+# If any transgressive heights are greater than emplacement depth, something is wrong, so drop 
+data = data[data.emplacement_depth > data.transgressive_height]
 
+# Sanity check by plotting EVERYTHING 
+data.hist("diameter")
+data.hist("midpoint_x")
+data.hist("midpoint_y")
+data.hist("emplacement_depth")
+data.hist("transgressive_height")
+data.plot(x="midpoint_x", y="midpoint_y", kind="scatter")
+data.plot(x="emplacement_depth", y="transgressive_height", kind="scatter").plot([0,8], [0,8], 'r-')
+
+# Write to CSV 
+data.to_csv("output.csv")
+# End of script
