ba6df0e08f88408fedad53d63f0506a0433bc376
Murray Hoggett <murrayhoggett@gmail.com>
2018-11-04 18:19:55 +0000

    Changed area density plot so now geographic so can use pscoast. Working.

--- sill_area_density.sh
+++ sill_area_density.sh
@@ -1,29 +1,31 @@
-#!/bin/bash 
+#!/bin/bash -x
 
+gmtset MAP_FRAME_TYPE plain
 file="oga_output_trans_decomp.csv"
-prj="-JX3i"
-awk -F"," '{if(NR>1)print $4,$5}' ${file} > file_whitespace.txt
-rgn=`gmtinfo file_whitespace.txt -I10000`
+prj="-JM3i"
+awk -F"," '{if(NR>1)print $4,$5}' ${file} > temp_sills_whitespace.txt
+# Convert sills into latlon
+cat temp_sills_whitespace.txt | mapproject -Ju+29/1:1 -I -C -F > sills_geog.txt
 
-# Convert survey lines to utm 
-mapproject `gmtinfo Rockall-UK_coords_geog.txt -I1` -C -F -Ju+29/1:1 Rockall-UK_coords_geog.txt > Rockall-UK_coords_utm29.txt
+rgnoga=`gmtinfo Rockall-UK_coords_geog.txt -I0.2`
 
-rgnoga=`gmtinfo Rockall-UK_coords_utm29.txt -I1000`
+# Output the count in every 10 km block.
+blockmean $rgnoga sills_geog.txt -I10000+e -C -Sn -fg > oga_counts_10000.txt
 
-# Output the count in every 10 km block. 
-blockmean $rgnoga file_whitespace.txt -I10000 -C -Sn > oga_counts_10000.txt
-
-# Grid the counts 
-nearneighbor $rgnoga oga_counts_10000.txt -I1000 -N6 -S30000 -Goga_counts_10000.nc
+# Grid the counts
+nearneighbor $rgnoga oga_counts_10000.txt -I1000+e -N6 -S30000+e -fg -Goga_counts_10000.nc
 
 # Divide values by 10 to get sill area density
 grdmath oga_counts_10000.nc 10 DIV = oga_counts_1000.nc
 
 
 # Make map
-grdimage $prj $rgnoga oga_counts_1000.nc -Bx200000 -By200000 -BSWne -Chot -K > oga_area_density.ps
-psxy $prj $rgnoga -W0.5 Rockall-UK_coords_utm29.txt -gd10000 -K -O >> oga_area_density.ps
+outfile=oga_area_density.ps
+makecpt -Chot -T0/0.8/0.1 -Z -D > sill_area_d.cpt
+grdimage $prj $rgnoga oga_counts_1000.nc -Bx2 -By2 -BSWne -Csill_area_d.cpt -K > $outfile
+pscoast -Gblack -Di $prj $rgnoga -K -O >> $outfile
+psxy $prj $rgnoga -W0.5 Rockall-UK_coords_geog.txt -gd5k -K -O >> $outfile
+psscale -D3.25i/1.2i/2i/0.25i -B0.2+l"Sill area density (km @+-1@+)" -Csill_area_d.cpt -O >> $outfile
 
-psscale -D3.25i/1.5i/2i/0.25i -B0.2+l"Sill area density (km @+-1@+)" -Chot -O >> oga_area_density.ps
-
-okular oga_area_density.ps
+psconvert -P -A0.5 $outfile
+eog *jpg
