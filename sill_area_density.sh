560aba9644d950e993c5641c2ab217405baaa50a
Murray Hoggett <murrayhoggett@gmail.com>
2018-11-05 18:53:39 +0000

    Added geological features

--- sill_area_density.sh
+++ sill_area_density.sh
@@ -4,6 +4,12 @@ gmtset MAP_FRAME_TYPE plain
 file="oga_output_trans_decomp.csv"
 linefile="Rockall-UK_coords_geog.txt"
 misc="-Bx2 -By2 -BSWne"
+ogr2ogr -F "GMT" faults_misc.gmt extra_data_rockall/faults_misc.shp
+ogr2ogr -F "GMT" folds_tuitt.gmt extra_data_rockall/folds_tuitt.shp
+ogr2ogr -F "GMT" volc_tuitt.gmt extra_data_rockall/volc_tuitt.shp
+
+
+
 sill_area_density()
 {
 prj="-JM3i"
@@ -44,6 +50,9 @@ grdimage oga_bathy.nc -Cbathy.cpt $prj $rgn -Y4i -K > $outfile
 grdcontour oga_bathy.nc -C250 $prj $rgn -Wgray10 -K -O >> $outfile
 pscoast $prj $rgn -Di -Gblack -K -O >> $outfile
 psscale -D2.75i/1i/-2i/0.25i -Cbathy.cpt -B1000 -K -O >> $outfile
+psxy $prj $rgn faults_misc.gmt -Sf0.25/0.25+r+f -W0.5,red -K -O >> $outfile
+psxy $prj $rgn folds_tuitt.gmt -Sf0.2/0.05+t -Gred -W0.5,red -K -O >> $outfile
+psxy $prj $rgn volc_tuitt.gmt -St0.2 -Gred -Wred -K -O >> $outfile
 psxy $prj $rgn $linefile -gd5k $misc -K -O >> $outfile
 
 # Gravity map
@@ -54,6 +63,9 @@ grdimage oga_bouguer20.nc -Cgrav.cpt $prj $rgn -X4i -K -O >> $outfile
 grdcontour oga_bouguer20.nc -C20 $prj $rgn -Wgray10 -K -O >> $outfile
 pscoast $prj $rgn -Di -Gblack -K -O >> $outfile
 psscale -D2.75i/1i/2i/0.25i -Cgrav.cpt -B25 -K -O >> $outfile
+psxy $prj $rgn faults_misc.gmt -Sf0.25/0.25+r+f -W0.5,red -K -O >> $outfile
+psxy $prj $rgn folds_tuitt.gmt -Sf0.2/0.05+t -Gred -W0.5,red -K -O >> $outfile
+psxy $prj $rgn volc_tuitt.gmt -St0.2 -Gred -Wred -K -O >> $outfile
 psxy $prj $rgn $linefile -gd5k $misc -K -O >> $outfile
 
 # Magnetic map
@@ -64,6 +76,9 @@ grdimage oga_rtpmaganomaly.nc -Cmag.cpt $prj $rgn -X-4i -Y-2.5i -K -O >> $outfil
 grdcontour oga_rtpmaganomaly.nc -C100 $prj $rgn -Wgray10 -K -O >> $outfile
 pscoast $prj $rgn -Di -Gblack -K -O >> $outfile
 psscale -D2.75i/1i/2i/0.25i -Cmag.cpt -B100 -K -O >> $outfile
+psxy $prj $rgn faults_misc.gmt -Sf0.25/0.25+r+f -W0.5,red -K -O >> $outfile
+psxy $prj $rgn folds_tuitt.gmt -Sf0.2/0.05+t -Gred -W0.5,red -K -O >> $outfile
+psxy $prj $rgn volc_tuitt.gmt -St0.2 -Gred -Wred -K -O >> $outfile
 psxy $prj $rgn $linefile -gd5k $misc -O >> $outfile
 
 # Legend
